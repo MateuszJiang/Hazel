7ed54af0f5b58a181e1a45f2a655b44654b7abc4
Matthew Caine <mateuszczylok1997@gmail.com>
2020-12-17 22:02:36 +0000

    Window support in Windows x64 system

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -8,7 +8,7 @@ namespace Hazel {
 
 	Application::Application()
 	{
-
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 
 	Application::~Application()
@@ -18,19 +18,10 @@ namespace Hazel {
 
 	void Application::Run()
 	{
-		WindowResizeEvent e(1280, 720);
-
-		if (e.IsInCategory(EventCategoryApplication))
+		while (m_Running)
 		{
-			HZ_TRACE(e);
+			m_Window->OnUpdate();
 		}
-
-		if (e.IsInCategory(EventCategoryInput))
-		{
-			HZ_TRACE(e);
-		}
-
-		while (1);
 	}
 
 }
