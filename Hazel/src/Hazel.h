415928586c9ce437333ab7850021bfdc6bebf6c9
Matthew Caine <mateuszczylok1997@gmail.com>
2020-12-20 21:08:09 +0000

    ImGui added as a layer

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -2,10 +2,11 @@
 
 // for use by hazel apps
 
-#include <stdio.h>
 #include "Hazel/Application.h"
 #include "Hazel/Layer.h"
 #include "Hazel/Log.h"
+
+#include "Hazel/ImGui/ImGuiLayer.h"
 
 // --------------- Entry Point
 #include "Hazel/EntryPoint.h"
